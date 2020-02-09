2a4fe4910e8f5a86a26bb50d4fe1c1a26fcc1610
joshua <jclaryea@gmail.com>
2020-02-09 13:52:29 +0000

    I've done the lesson and lesson activity Pls give feedback on whatsapp

--- app/src/main/java/com/welearn/wemath/Lesson.java
+++ app/src/main/java/com/welearn/wemath/Lesson.java
@@ -1,4 +1,39 @@
 package com.welearn.wemath;
 
+import android.media.Image;
+
 public class Lesson {
+    private String mTitle;
+    private String mContent;
+    private String mImage;
+
+    Lesson(String title, String content, String image){
+        mTitle = title;
+        mContent = content;
+        mImage = image;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public void setImage(String image) {
+        mImage = image;
+    }
 }
--- app/src/main/java/com/welearn/wemath/LessonActivity.java
+++ app/src/main/java/com/welearn/wemath/LessonActivity.java
@@ -2,13 +2,25 @@ package com.welearn.wemath;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LessonActivity extends AppCompatActivity {
+    private Lesson mLesson;
+    private TextView mTitle;
+    private TextView mContent;
+    private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson);
+
+        mLesson = new Lesson("Title", "Content", "C:\\Users\\hp\\AndroidStudioProjects\\math-application\\app\\src\\main\\res\\drawable\\fblogo.png");
+        mTitle= findViewById(R.id.title);
+        mContent = findViewById(R.id.content);
+        mImage = findViewById(R.id.imageView);
     }
 }
