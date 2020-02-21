36a230e06db49c14017d2d5bcf771298db58a6b8
Lanciné <l.conde@student.lancaster.edu.gh>
2020-02-21 16:12:30 +0000

    Filenames fixed

--- app/src/main/java/com/welearn/wemath/QuizMainFragment.java
+++ app/src/main/java/com/welearn/wemath/QuizMainFragment.java
@@ -5,29 +5,18 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link QuizMainFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link QuizMainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class QuizMainFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageButton mAnswerButton;
+    private ImageButton mCreateButton;
 
     private OnFragmentInteractionListener mListener;
 
@@ -35,38 +24,34 @@ public class QuizMainFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QuizMainFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static QuizMainFragment newInstance(String param1, String param2) {
         QuizMainFragment fragment = new QuizMainFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz_main, container, false);
+        View root =  inflater.inflate(R.layout.fragment_quiz_main, container, false);
+
+        mAnswerButton = root.findViewById(R.id.quiz_answering_image);
+        mCreateButton = root.findViewById(R.id.quiz_creation_image);
+
+        mAnswerButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_quiz_main_to_quizTakingMenuFragment, null));
+
+        return root;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/welearn/wemath/quizzes/AdminQuizSelectionFragment.java
+++ app/src/main/java/com/welearn/wemath/quizzes/AdminQuizSelectionFragment.java
@@ -4,17 +4,19 @@ package com.welearn.wemath.quizzes;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.welearn.wemath.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class AdminQuizSelectionFragment extends Fragment {
+
+
 
 
     public AdminQuizSelectionFragment() {
@@ -26,7 +28,10 @@ public class AdminQuizSelectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_quiz_selection, container, false);
+        View root = inflater.inflate(R.layout.fragment_admin_quiz_selection, container, false);
+
+
+        return root;
     }
 
 }
