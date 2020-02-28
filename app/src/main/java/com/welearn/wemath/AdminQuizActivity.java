b0981ddf2ad0036ff6e91957e1eb71328571a364
Lanciné <l.conde@student.lancaster.edu.gh>
2020-02-28 16:22:04 +0000

    AdminQuizActivity added

---
+++ app/src/main/java/com/welearn/wemath/AdminQuizActivity.java
@@ -0,0 +1,14 @@
+package com.welearn.wemath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_quiz);
+    }
+}
